fc35d206fc9c136df21496e442acb1edd1bc73b0
Mark Carter <magcarter@hotmail.co.uk>
2023-09-12 09:27:30 +0100

    ImageUploader Completed with basic functionality to upload mutiple to images to folder in root.

--- TheWhaddonShowReact/ClientApp/src/components/ImageUploader/ImageUploader.js
+++ TheWhaddonShowReact/ClientApp/src/components/ImageUploader/ImageUploader.js
@@ -4,93 +4,124 @@ import { uploadFiles } from '../DataAccess/DataAccess';
 import {
     FormGroup,
     Label,
+    Row,
     Col,
     Button,
-    Input
+    Input,
+    Container
 } from 'reactstrap';
+import Widget from 'components/Widget'
+import Dropzone from 'react-dropzone'
+import s from 'pages/forms/elements/Elements.module.scss';
 export default class ImageUploader extends React.Component {
+
+    static defaultProps = {
+        singleFile: false,
+        inputTags: true,
+        dropZoneText: 'This dropzone accepts only images. Try dropping some here, or click to select files to upload.'
+    };
 
     constructor(props) {
         super(props);
 
-        this.searchTags = true
-        this.aria = true
-
         this.state = {
             images: []
+            ,searchTags: []
         };
     }
 
-
-
-    handleImageChange = (event) => {
-
-        const imagesArray = Array.from(event.target.files)
-
-        console.log(imagesArray)
-
+    handleDrop = (selectedFiles) => {
         this.setState({
-            images: imagesArray
-        });
+            images: [...this.state.images, ...selectedFiles]
+        })
     }
 
+    handleTagsChange = (event) => {
+        this.setState({ searchTags: event.target.value })
+    }
+
+
     handleUploadClick = async () => {
-        await uploadFiles(this.state.images, 'test')
-        this.setState({ images: [] })
-    } 
-
-            render() {
-                return (
-                    <>
-                        <FormGroup row>
-                            <Label md="4" className="text-md-right">
-                                Image upload
-                            </Label>
-                            <Col md="8">
-                                <input
-                                    accept="image/*" onChange={this.handleImageChange}
-                                    id="selectImage"
-                                    type="file" name="file" className="display-none"
-                                />
-                                <p>{this.state.images.length}</p>
-                                <div className="fileinput fileinput-new fileinput-fix">
-                                    <div className="fileinput-new thumbnail">
-                                        {this.state.images.length > 0 ? <div>
-                                            {this.state.images.map((image, idx) => (
-                                                <img alt="..." src={image.preview} key={`img-id-${idx.toString()}`} />))}
-                                        </div> : <img
-                                            alt="..."
-                                            src="data:image/svg+xml;base64,PHN2ZyB4bWxucz0iaHR0cDovL3d3dy53My5vcmcvMjAwMC9zdmciIHdpZHRoPSIxOTEiIGhlaWdodD0iMTQxIj48cmVjdCB3aWR0aD0iMTkxIiBoZWlnaHQ9IjE0MSIgZmlsbD0iI2VlZSIvPjx0ZXh0IHRleHQtYW5jaG9yPSJtaWRkbGUiIHg9Ijk1LjUiIHk9IjcwLjUiIHN0eWxlPSJmaWxsOiNhYWE7Zm9udC13ZWlnaHQ6Ym9sZDtmb250LXNpemU6MTJweDtmb250LWZhbWlseTpBcmlhbCxIZWx2ZXRpY2Esc2Fucy1zZXJpZjtkb21pbmFudC1iYXNlbGluZTpjZW50cmFsIj4xOTF4MTQxPC90ZXh0Pjwvc3ZnPg=="
-                                        />}
-                                    </div>
-                                </div>
-                                <div>
-                                    <Button type="button" color="gray-default"><Label for="selectImage">Select image</Label></Button>
-                                </div>
-                            </Col>
-                            {(this.state.searchTags) &&
-                                <>
-                                    <Label for="search-tags" md={4} className="text-md-right">
-                                        Search Tags
-                                    </Label>
-
-                                    <div>
-                                        <Input type="text" id="search-tags" placeholder="e.g. 2023 Band Rehearsal" />
-                                        <span className="help-block">Separate tags with a space or leave blank.</span>
-                                    </div>
-                                </>
-                            }
-                            {(this.state.images.length > 0) &&
-                                <div>
-                                    <Button type="button" color="gray-default"><Label for="image-upload" onClick={this.handleUploadClick} >Upload image</Label></Button>
-                                </div>
-
-                            }
-                        </FormGroup>
-
-
-                    </>
-                );
-            }
+        if (this.state.images.length === 0) {
+            alert('Please select at least 1 file to upload.')
+        } else {
+            await uploadFiles(this.state.images, 'test')
+            this.setState({ images: [] })
+            this.setSate({ searchTags: [] })
         }
+    }
+
+    handleClearClick = () => {
+        this.setState({ images: [] })
+        this.setState({ searchTags: []})
+    }
+
+    render() {
+        
+        const { singleFile, inputTags, dropZoneText } = this.props;
+
+        const { images,searchTags } = this.state;
+
+        return (
+
+
+            <Widget
+                title={<h6>Upload <strong> image{(singleFile) ? "" : "s"}</strong></h6>} collapse
+            >
+                <Container className="d-flex align-items- justify-content-center ">
+                    <FormGroup>
+                        <Row>
+                            <Dropzone
+                                onDrop={this.handleDrop} accept="image/*"
+                                className={`${s.dropzone} dropzone`}
+                            >
+                                {images.length > 0 ?
+
+                                    <Row>
+                                        {images.map((image, idx) => (
+                                            <Col>
+
+                                                <div className="display-inline-block me-2 mb-xs" key={`drop-id-${idx.toString()}`}>
+                                                    <img alt="..." src={URL.createObjectURL(image)} className="img-fluid" />
+                                                    <div>{image.name}</div>
+                                                </div>
+                                            </Col>
+                                        ))}
+                                    </Row>
+
+                                    : <p>{dropZoneText}</p>}
+                            </Dropzone>
+                        </Row>
+                        {(inputTags) &&
+                            <Row>
+
+                                <div className="my-3">
+                                    <Label for="search-tags" md={4} className="text-md-right mb-1">
+                                        Search Tags 
+                                    </Label>
+                                   
+                                    <div>
+                                        
+                                        <Input type="text" id="search-tags" placeholder="e.g. 2023 Band Rehearsal (or leave blank)" value={searchTags} onChange={this.handleTagsChange} />
+                                        <span className="help-block"> (These will be added as tags to the image for searching etc.)</span>
+                                    </div>
+                                </div>
+
+                            </Row>
+                        }
+                        <Row>
+                            <Button type="button" color="primary" onClick={this.handleUploadClick} disabled={images.length === 0}>Upload images</Button>
+                        </Row>
+                        {(images.length > 0) &&
+                            <Row>
+                                <Button type="button" color="danger" onClick={this.handleClearClick} disabled={images.length === 0}>Clear selection</Button>
+                            </Row>
+                        }
+                    </FormGroup >
+                </Container>
+            </Widget >
+
+        );
+    }
+}
 
