1608486f35f0cf7e8d296d6c3bf96162775db1df
Mark Carter <magcarter@hotmail.co.uk>
2023-10-30 12:03:42 +0000

    Changes to Controllers and Models for ScriptImporter, ScriptItem and Parts

---
+++ TheWhaddonShowReact/Models/LocalServer/ImportLine.cs
@@ -0,0 +1,11 @@
+﻿namespace TheWhaddonShowReact.Models.LocalServer
+{
+	public class ImportLine
+	{
+		public string Type { get; set; } = "";
+		public string Text { get; set; } = "";
+		public List<string> Parts { get; set; } = new List<string>();
+
+
+	}
+}
---
+++ TheWhaddonShowReact/Models/LocalServer/ImportResponse.cs
@@ -0,0 +1,18 @@
+﻿using TheWhaddonShowClassLibrary.Models;
+
+namespace TheWhaddonShowReact.Models.LocalServer
+{
+	public class ImportResponse
+	{
+		public List<PartUpdate> PartUpdates { get; set; } = new List<PartUpdate>();
+
+		public List<ScriptItemUpdate> ScriptItemUpdates { get; set; } = new List<ScriptItemUpdate>();
+
+
+		public ImportResponse(List<PartUpdate> partUpdates, List<ScriptItemUpdate> scriptItemUpdates)
+		{
+			PartUpdates = partUpdates;
+			ScriptItemUpdates = scriptItemUpdates;
+		}
+	}
+}
