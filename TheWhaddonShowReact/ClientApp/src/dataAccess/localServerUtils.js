21c55d21d86b369d42b7f1e716fe7a866e269189
Mark Carter <magcarter@hotmail.co.uk>
2023-11-14 12:10:26 +0000

    Refactor of UseEffect and sync method in useSync completed. ScriptItem and Person tested.

--- TheWhaddonShowReact/ClientApp/src/dataAccess/localServerUtils.js
+++ TheWhaddonShowReact/ClientApp/src/dataAccess/localServerUtils.js
@@ -4,12 +4,11 @@
 import axios from 'axios';
 
 //React Hooks
-import { useState, useEffect, useRef } from 'react';
+import { useState, useEffect } from 'react';
 
 
 //LocalServerModels and types
-import { LocalToServerSyncData } from './localServerModels';
-import { Person, ScriptItem, Part } from './localServerModels';
+import { LocalToServerSyncData, SCRIPT_ITEM, PERSON, PART } from './localServerModels';
 
 //Redux Hooks
 import { useDispatch, useSelector } from 'react-redux';
@@ -22,8 +21,10 @@ import {
     clearConflicts,
     //updateConnectionStatus,
     endSync,
-    closePostBack
+    closePostBack,
+    setPauseSync
 } from '../actions/localServer';
+
 import { log } from '../helper.js';
 
 import { IMPORT_GUID } from '../pages/scriptEditor/ScriptImporter';
@@ -53,212 +54,247 @@ export async function useSync() {
 
     //Use Effect Hooks to assing the data and url to be used in the sync operation. **LSMTypeInCode**
     //-------------------------------------------------------------------------------  
-    useEffect(() => {
-        debug && console.log('setting data and type')
-        if (persons.sync.isSyncing) { //if it is syncing alread then don't run another sync.
-            setData(persons.history)
-            setType(persons.type)
-            setTriggerSync(!triggerSync)
-        }
-        //else do nothing
-    }, [persons.sync.isSyncing]) // eslint-disable-line react-hooks/exhaustive-deps
 
     useEffect(() => {
-        if (scriptItems.sync.isSyncing) {
-            setData(scriptItems.history.filter(item => item.parentId !== IMPORT_GUID))
-            setType(scriptItems.type)
-            setTriggerSync(!triggerSync)
+
+        log(debug, `${PERSON} isSyncing: ${persons.sync.isSyncing}`)
+
+        const runSync = async () => {
+
+            if (pauseSync === true) finishSync('Syncing paused.', PERSON)
+
+            if (persons.sync.isSyncing) {
+
+                const { error } = await sync(persons.history, PERSON)
+
+                finishSync(error, PERSON)
+
+            }
+
         }
-        //else do nothing
+
+        runSync()
+
+    }, [persons.sync.isSyncing]) // eslint-disable-line react-hooks/exhaustive-dep
+
+    useEffect(() => {
+
+        log(debug, `${SCRIPT_ITEM} isSyncing: ${scriptItems.sync.isSyncing}`)
+
+        const runSync = async () => {
+
+            if (pauseSync === true) finishSync('Syncing paused.', SCRIPT_ITEM)
+
+            if (scriptItems.sync.isSyncing) {
+
+                const { error } = await sync(scriptItems.history, SCRIPT_ITEM)
+
+                finishSync(error, SCRIPT_ITEM)
+
+            }
+
+        }
+
+        runSync()
+
     }, [scriptItems.sync.isSyncing]) // eslint-disable-line react-hooks/exhaustive-deps
 
     useEffect(() => {
-        if (parts.sync.isSyncing) {
-            setData(parts.history)
-            setType(parts.type)
-            setTriggerSync(!triggerSync)
+
+        log(debug, `${PART} isSyncing: ${parts.sync.isSyncing}`)
+
+        const runSync = async () => {
+
+            if (pauseSync === true) finishSync('Syncing paused.', PART)
+
+            if (parts.sync.isSyncing) {
+
+                const { error } = await sync(parts.history, PART)
+
+                finishSync(error, PART)
+
+            }
+
         }
-        //else do nothing
+
+        runSync()
+
     }, [parts.sync.isSyncing]) // eslint-disable-line react-hooks/exhaustive-deps
 
 
 
     //The actual sync operation
     //-------------------------------------------------------------------------------
-    useEffect(() => {
+    const sync = async(data, type) => {
 
-        debug && console.log(data)
-        async function sync() {
+        let error;
 
-            try {
+        try {
 
-                const syncData = await createSyncData(data, localCopyId, debug)
+            const syncData = await createSyncData(data, localCopyId)
 
-                const syncResponse = await postSyncData(syncData, type, dispatch, debug)
+            const syncResponse = await postSyncData(syncData, type)
 
-                if (syncResponse.status === 200) {
-                    const cbpSuccess = await closePostBacks(syncResponse.data.postBacks, type, dispatch, debug)
+            if (syncResponse.status === 200) {
+                const cbpSuccess = await closePostBacks(syncResponse.data.postBacks, type)
 
-                    const psrSuccess = await processSyncResponse(syncResponse.data, type, dispatch, debug)
+                const psrSuccess = await processSyncResponse(syncResponse.data, type)
 
-                    if (cbpSuccess && psrSuccess) {
-                        finishSync(null, type, dispatch)
-                    } else {
-                        finishSync('Error finishing sync.', null, type, dispatch, debug)
-                    }
-
-
+                if (cbpSuccess && psrSuccess) {
+                    error = null
                 } else {
-                    finishSync(`Error: ${syncResponse.message}`, type, dispatch)
-
+                    error = 'Error finishing sync.'
                 }
 
-            } catch (error) {
-                debug && console.log(error)
-                finishSync(error.message, type, dispatch)
+
+            } else {
+                error = `Error: ${syncResponse.message}`
+
             }
+
+        } catch (error) {
+            error = ('sync Error: ' + error.message)
+        }
+
+        return { error }
+
+    }
+
+
+    const createSyncData = async (data, copyId) => { //data = all the updates pertaining to a particular type of data (e.g. persons)
+
+        debug && console.log('Redux store data: ')
+
+        debug && console.log(data)
+
+        try {
+            const syncData = new LocalToServerSyncData(
+                copyId  //identifies the local copy that the data is coming from
+                , data.filter(x => x.hasPostedBack !== true).map(x => ({ id: x.id, created: x.created, isConflicted: x.isConflicted })) // confirmation back to server that updates in the post back have been added to Local.
+                , data.filter(x => x.updatedOnServer === null) //local data that hasn't yet been added to server
+            )
+
+            return syncData;
+        } catch (error) {
+            throw new Error(`Error creating sync data: ${error.message}`)
+        }
+
+    }
+
+
+
+
+    const postSyncData = async (syncData, type, dispatch, debug) => {
+
+        const url = `${type}s/sync`
+
+        try {
+
+            log(debug, "postSyncData: ", { type, syncData });
+
+
+            log(debug, 'postSyncData: url', url)
+            const response = await axios.post(url, syncData);
+
+            log(debug, 'postSyncData response: ', { status: response.status, data: response.data })
+
+            //dispatch(updateConnectionStatus('Ok'))
+
+            return (response)
+        }
+        catch (error) {
+            throw new Error("Error posting sync data: " + error.message)
+        }
+    }
+
+    const closePostBacks = (postBacks, type) => {
+
+        log(debug, 'sync closePostBacks: ', { postBacks: postBacks, type: type })
+
+        try {
+            const postBacksArray = Object.values(postBacks)
+
+            postBacksArray.forEach(postBack => { dispatch(closePostBack(postBack, type)) })
+
+            return true;
+        }
+        catch (error) {
+            throw new Error("Error closing postBacks: " + error.message)
+        }
+    }
+
+    const processSyncResponse = async (responseData, type) => {
+
+        let process = ''
+        log(debug, 'processSyncResponse:', { responseData, type })
+
+        try {
+            process = 'postBacks'
+            if (responseData.postBacks.length === 0) {
+
+                debug && console.log('No PostBack to process.');
+                debug && console.log(type)
+
+            }
+            else {
+                debug && console.log('Processing postBacks.')
+                debug && console.log(type)
+
+                const postBacksArray = Object.values(responseData.postBacks)
+
+                postBacksArray.forEach(postBack => { dispatch(processServerToLocalPostBacks(postBack, type)) })
+            }
+
+            process = 'updates'
+            if (responseData.updates.length === 0) {
+                debug && console.log('No updates to process.');
+                debug && console.log(type)
+            }
+            else {
+                debug && console.log('Processing updates.');
+                debug && console.log(type)
+                dispatch(addUpdates(responseData.updates, type))
+            }
+
+            process = 'conflicts'
+            if (responseData.conflictIdsToClear.length === 0) {
+                debug && console.log('No conflicts to clear.');
+                debug && console.log(type)
+
+            }
+            else {
+                debug && console.log('Clearing conflicts.')
+                dispatch(clearConflicts(responseData.conflictIdsToClear))
+            }
+
+            process = 'lastSyncDate'
+            if (responseData.lastSyncDate == null) {
+                debug && console.log('No last sync date to update.')
+            }
+
+            else {
+                debug && console.log('Updating last sync date.')
+                dispatch(updateLastSyncDate(responseData.lastSyncDate))
+            }
+
+            return true;
+        }
+        catch (err) {
+            throw new Error(`Error processing ${process} from server: ${err.message}`)
         }
 
 
-        sync()
 
+    }
 
-    }, [triggerSync]) // eslint-disable-line react-hooks/exhaustive-deps
-
-
-}
-
-const createSyncData = async (data, copyId, debug) => { //data = all the updates pertaining to a particular type of data (e.g. persons)
-
-    debug && console.log('Redux store data: ')
-
-    debug && console.log(data)
-
-    try {
-        const syncData = new LocalToServerSyncData(
-            copyId  //identifies the local copy that the data is coming from
-            , data.filter(x => x.hasPostedBack !== true).map(x => ({ id: x.id, created: x.created, isConflicted: x.isConflicted })) // confirmation back to server that updates in the post back have been added to Local.
-            , data.filter(x => x.updatedOnServer === null) //local data that hasn't yet been added to server
-        )
-
-        return syncData;
-    } catch (error) {
-        throw new Error(`Error creating sync data: ${error.message}`)
+    const finishSync = (error, type) => {
+        if (error) {
+            log(debug, `Error syncing ${type}: ${error}`)
+        }
+        dispatch(endSync(error, type))
     }
 
 }
-
-
-
-
-const postSyncData = async (syncData, type, dispatch, debug) => {
-
-    const url = `${type}s/sync`
-
-    try {
-
-        log(debug, "postSyncData: ", { type, syncData });
-
-      
-        log(debug, 'postSyncData: url', url)
-        const response = await axios.post(url, syncData);
-
-        log(debug, 'postSyncData response: ', { status: response.status, data: response.data })
-
-        //dispatch(updateConnectionStatus('Ok'))
-
-        return (response)
-    }
-    catch (error) {
-        throw new Error("Error posting sync data: " + error.message)
-    }
-}
-
-const closePostBacks = (postBacks, type, dispatch, debug) => {
-
-    log(debug, 'sync closePostBacks: ', { postBacks: postBacks, type: type })
-
-    try {
-        const postBacksArray = Object.values(postBacks)
-
-        postBacksArray.forEach(postBack => { dispatch(closePostBack(postBack, type)) })
-
-        return true;
-    }
-    catch (error) {
-        throw new Error("Error closing postBacks: " + error.message)
-    }
-}
-
-const processSyncResponse = async (responseData, type, dispatch, debug) => {
-
-    let process = ''
-    log(debug, 'processSyncResponse:', { responseData, type })
-
-    try {
-        process = 'postBacks'
-        if (responseData.postBacks.length === 0) {
-
-            debug && console.log('No PostBack to process.');
-            debug && console.log(type)
-
-        }
-        else {
-            debug && console.log('Processing postBacks.')
-            debug && console.log(type)
-
-            const postBacksArray = Object.values(responseData.postBacks)
-
-            postBacksArray.forEach(postBack => { dispatch(processServerToLocalPostBacks(postBack, type)) })
-        }
-
-        process = 'updates'
-        if (responseData.updates.length === 0) {
-            debug && console.log('No updates to process.');
-            debug && console.log(type)
-        }
-        else {
-            debug && console.log('Processing updates.');
-            debug && console.log(type)
-            dispatch(addUpdates(responseData.updates, type))
-        }
-
-        process = 'conflicts'
-        if (responseData.conflictIdsToClear.length === 0) {
-            debug && console.log('No conflicts to clear.');
-            debug && console.log(type)
-
-        }
-        else {
-            debug && console.log('Clearing conflicts.')
-            dispatch(clearConflicts(responseData.conflictIdsToClear))
-        }
-
-        process = 'lastSyncDate'
-        if (responseData.lastSyncDate == null) {
-            debug && console.log('No last sync date to update.')
-        }
-
-        else {
-            debug && console.log('Updating last sync date.')
-            dispatch(updateLastSyncDate(responseData.lastSyncDate))
-        }
-
-        return true;
-    }
-    catch (err) {
-        throw new Error(`Error processing ${process} from server: ${err.message}`)
-    }
-
-
-
-}
-
-const finishSync = (error, type, dispatch,debug) => {
-    log(debug,'finishSync: ', { error, type })
-    dispatch(endSync(error, type))
-}
-
 
 export function getLatest(history, undoDateTime = null, includeInActive = false, includeSamples = false) {
     const undoDate = (undoDateTime) ? new Date(undoDateTime) : null
