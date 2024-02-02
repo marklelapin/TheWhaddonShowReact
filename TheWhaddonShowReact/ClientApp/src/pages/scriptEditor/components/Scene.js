837065222d727f339a6ac1f6af25783b948ae15d
Mark Carter <magcarter@hotmail.co.uk>
2024-02-02 10:14:28 +0000

    Next and Previous Buttons Added for Mobile

--- TheWhaddonShowReact/ClientApp/src/pages/scriptEditor/components/Scene.js
+++ TheWhaddonShowReact/ClientApp/src/pages/scriptEditor/components/Scene.js
@@ -3,17 +3,20 @@ import React, { memo } from 'react';
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
 
-import { trigger, ADD_SCENE, updateSceneLoaded } from '../../../actions/scriptEditor'
+import { trigger, ADD_SCENE, updateSceneLoaded, updateScriptFilter } from '../../../actions/scriptEditor'
 
 //Components
 import ScriptItem from '../../../pages/scriptEditor/components/ScriptItem.js';
 import PartEditor from '../../../pages/scriptEditor/components/PartEditor.js';
-
+import Hammer from 'rc-hammerjs';
+import { Icon } from '../../../components/Icons/Icons';
+import { Button } from 'reactstrap';
 //Utilities
 import { log, SCENE as logType } from '../../../dataAccess/logging'
 import { partEditorRowId } from '../scripts/part';
 import classnames from 'classnames';
 import { isScriptReadOnly } from '../../../dataAccess/userAccess';
+import { moveFocusToId } from '../scripts/utility';
 //styling
 import s from '../Script.module.scss'
 
@@ -51,7 +54,7 @@ const Scene = memo((props) => {
 
     const scriptFilter = useSelector(state => state.scriptEditor.scriptFilter)
 
-    let sceneFilter  
+    let sceneFilter
     if (scriptFilter === null || scriptFilter === undefined) {
         sceneFilter = isMobileDevice ? (sceneNumber === 1) : true
     } else {
@@ -68,103 +71,154 @@ const Scene = memo((props) => {
 
     log(logType, 'scene', scene)
 
-    return (
-        <>
-            <div id={`scene-${scene.id}`} className={classnames(s[`scene-group`], s[scene.type.toLowerCase()], (sceneFilter) ? null : s['hide'])} style={{ zIndex: zIndex }}>
-                <div className={s[`scene-header`]}>
-                    {(scene) &&
-                        <ScriptItem
-                            id={scene.id}
-                            key={scene.id}
-                            sceneId={scene.id}
-                            zIndex={scene.zIndex}
-                            curtainOpen={previousCurtainOpen}
-                            previousFocusId={scene.previousFocusId}
-                            nextFocusId={scene.nextFocusId}
-                            sceneNumber={sceneNumber}
-                        />
-
-                    }
-                    {scene.type === SCENE && synopsis &&
-                        <ScriptItem
-                            id={synopsis.id}
-                            key={synopsis.id}
-                            sceneId={scene.id}
-                            zIndex={synopsis.zIndex}
-                            curtainOpen={previousCurtainOpen}
-                            previousFocusId={synopsis.previousFocusId}
-                            nextFocusId={partEditorRowId(synopsis.nextFocusId, scene.id)}
-                        />
-                    }
-                    {(scene.type === SCENE) &&
-                        <PartEditor
-                            sceneId={scene.id}
-                            zIndex={synopsis.zIndex - 1}
-                            curtainOpen={previousCurtainOpen}
-                            previousFocusId={synopsis.id} //override the default focus ids
-                            nextFocusId={staging.id}
-
-                        />
-                    }
-                    {scene.type === SCENE && staging &&
-                        <>
-                            <ScriptItem
-                                id={staging.id}
-                                key={staging.id}
-                                sceneId={scene.id}
-                                zIndex={staging.zIndex}
-                                curtainOpen={previousCurtainOpen}
-                                previousFocusId={partEditorRowId(staging.previousFocusId, scene.id)}
-                                nextFocusId={staging.nextFocusId}
-                            />
-                        </>
-
-                    }
-
-                </div>
 
 
 
-                <div className={s['scene-body']}>
-                    {bodyOrder.map((scriptItem, idx) => {
-                        return (
-                            <ScriptItem
-                                id={scriptItem.id}
-                                key={scriptItem.id}
-                                sceneId={scene.id}
-                                zIndex={scriptItem.zIndex}
-                                previousCurtainOpen={bodyOrder[idx - 1]?.curtainOpen || previousCurtainOpen}
-                                curtainOpen={scriptItem.curtainOpen}
-                                alignRight={scriptItem.alignRight}
-                                isViewAsPartPerson={scriptItem.isViewAsPartPerson}
-                                previousFocusId={scriptItem.previousFocusId}
-                                nextFocusId={(idx === bodyOrder.length - 1) ? scene.nextSceneId : scriptItem.nextFocusId}
-                            />
-                        )
-                    })
-                    }
-                </div>
-
-
-
-            </div>
-            {scene.type !== SHOW &&
-                <div id={`scene-footer-${scene.id}`}
-                    className={classnames(
-                        s['scene-footer'],
-                        (scriptFilter) ? s.scriptFilterOn : null,
-                        sceneFilter ? null : s.hide
-                    )}
-                >{(!scriptFilter && !readOnly) &&
-                    <div key={`add-scene-${scene.id}`} className={classnames(s['add-new-scene'], s[viewStyle], (readOnly) ? null : 'clickable')} onClick={() => dispatch(trigger(ADD_SCENE, { scriptItem: sceneScriptItem }))}>
-                        (add new scene)
-                    </div>
-                    }
-
-                    {/*  <CurtainBackground curtainOpen={finalScriptItem.curtainOpen} />*/}
-                </div>
+    const handleSwipe = (e) => {
+        if ('ontouchstart' in window) {
+            if (e.direction === 4 && scene.nextId) {
+                moveToNextScene; return;
             }
-        </>
+
+            if (e.direction === 2 && scene.previousId) {
+                moveToPreviousScene; return;
+            }
+        }
+    }
+
+
+    const moveToNextScene = () => {
+        if (scene.nextSceneId) {
+            dispatch(updateScriptFilter([scene.nextSceneId]));
+            moveFocusToId(scene.id);
+        }
+    }
+
+    const moveToPreviousScene = () => {
+        if (sceneNumber !== 1) {
+            dispatch(updateScriptFilter([scene.previousId]));
+            moveFocusToId(scene.id);
+        }
+    }
+
+    return (
+        <Hammer onSwipe={handleSwipe}>
+            <>
+                <div id={`scene-${scene.id}`} className={classnames(s[`scene-group`], s[scene.type.toLowerCase()], (sceneFilter) ? null : s['hide'])} style={{ zIndex: zIndex }}>
+                    {(scriptFilter?.length === 1) &&
+                        <>
+                            {(sceneNumber !== 1) && <div className={classnames(s.previousSceneIcon,'clickable')} onClick={moveToPreviousScene} > <Icon icon='arrow-left' /></div>
+                            }
+                            {(scene.nextSceneId !==null) && <div className={classnames(s.nextSceneIcon,'clickable')} onClick={moveToNextScene} > <Icon icon='arrow-right' /></div>
+                            }
+                        </>
+                    }
+
+
+                    <div className={s[`scene-header`]}>
+                        {(scene) &&
+                            <ScriptItem
+                                id={scene.id}
+                                key={scene.id}
+                                sceneId={scene.id}
+                                zIndex={scene.zIndex}
+                                curtainOpen={previousCurtainOpen}
+                                previousFocusId={scene.previousFocusId}
+                                nextFocusId={scene.nextFocusId}
+                                sceneNumber={sceneNumber}
+                            />
+
+                        }
+                        {scene.type === SCENE && synopsis &&
+                            <ScriptItem
+                                id={synopsis.id}
+                                key={synopsis.id}
+                                sceneId={scene.id}
+                                zIndex={synopsis.zIndex}
+                                curtainOpen={previousCurtainOpen}
+                                previousFocusId={synopsis.previousFocusId}
+                                nextFocusId={partEditorRowId(synopsis.nextFocusId, scene.id)}
+                            />
+                        }
+                        {(scene.type === SCENE) &&
+                            <PartEditor
+                                sceneId={scene.id}
+                                zIndex={synopsis.zIndex - 1}
+                                curtainOpen={previousCurtainOpen}
+                                previousFocusId={synopsis.id} //override the default focus ids
+                                nextFocusId={staging.id}
+
+                            />
+                        }
+                        {scene.type === SCENE && staging &&
+                            <>
+                                <ScriptItem
+                                    id={staging.id}
+                                    key={staging.id}
+                                    sceneId={scene.id}
+                                    zIndex={staging.zIndex}
+                                    curtainOpen={previousCurtainOpen}
+                                    previousFocusId={partEditorRowId(staging.previousFocusId, scene.id)}
+                                    nextFocusId={staging.nextFocusId}
+                                />
+                            </>
+
+                        }
+
+                    </div>
+
+
+
+                    <div className={s['scene-body']}>
+                        {bodyOrder.map((scriptItem, idx) => {
+                            return (
+                                <ScriptItem
+                                    id={scriptItem.id}
+                                    key={scriptItem.id}
+                                    sceneId={scene.id}
+                                    zIndex={scriptItem.zIndex}
+                                    previousCurtainOpen={bodyOrder[idx - 1]?.curtainOpen || previousCurtainOpen}
+                                    curtainOpen={scriptItem.curtainOpen}
+                                    alignRight={scriptItem.alignRight}
+                                    isViewAsPartPerson={scriptItem.isViewAsPartPerson}
+                                    previousFocusId={scriptItem.previousFocusId}
+                                    nextFocusId={(idx === bodyOrder.length - 1) ? scene.nextSceneId : scriptItem.nextFocusId}
+                                />
+                            )
+                        })
+                        }
+                    </div>
+
+
+
+                </div>
+                {scene.type !== SHOW &&
+                    <div id={`scene-footer-${scene.id}`}
+                        className={classnames(
+                            s['scene-footer'],
+                            (scriptFilter) ? s.scriptFilterOn : null,
+                            sceneFilter ? null : s.hide
+                        )}
+                    >
+                        {(!scriptFilter && !readOnly) &&
+                            <div key={`add-scene-${scene.id}`} className={classnames(s['add-new-scene'], s[viewStyle], (readOnly) ? null : 'clickable')} onClick={() => dispatch(trigger(ADD_SCENE, { scriptItem: sceneScriptItem }))}>
+                                (add new scene)
+                            </div>
+                        }
+                        {(scriptFilter?.length === 1) &&
+                            < div className={s['next-previous-scene-buttons']}>
+                                {sceneNumber !== 1 && < Button onClick={moveToPreviousScene}>Previous Scene</Button>}
+                                {sceneNumber === 1 && <p>Start</p>}
+                                {(scene.nextSceneId) && <Button onClick={moveToNextScene}>Next Scene</Button>}
+                                {(!scene.nextSceneId) && <p>End</p>}
+                            </div>
+                        }
+
+                    </div >
+                }
+
+            </>
+        </Hammer >
     )
 }
 )
