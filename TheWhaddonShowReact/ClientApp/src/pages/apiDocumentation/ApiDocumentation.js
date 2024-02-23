4b07f4ff4f123d3bd9a53323d4db3e5311398b68
Mark Carter <magcarter@hotmail.co.uk>
2024-02-23 08:10:36 +0000

    ApiDocumentationImage Added

--- TheWhaddonShowReact/ClientApp/src/pages/apiDocumentation/ApiDocumentation.js
+++ TheWhaddonShowReact/ClientApp/src/pages/apiDocumentation/ApiDocumentation.js
@@ -1,8 +1,8 @@
 import React from 'react';
 /*import { useSelector } from 'react-redux';*/
+import theWhaddonShowApiSnapshot from '../../images/the-whaddon-show-api-snapshot.png';
 
-
-
+import classnames from 'classnames'
 //Components
 
 import s from './ApiDocumentation.module.scss';
@@ -17,8 +17,8 @@ const ApiDocumentation = () => {
             <p>The Whaddon Show App is built with a React frontend and c#/.Net backend.</p>
             <p>{`This in turn accesses a public facing API giving role based read and write permissions to The Whaddon Show's central SQL database with authentication managed through Azure AdB2C`}</p>
             <p>Check out the <a href="https://thewhaddonshowapi.azurewebsites.net">Whaddon Show APIs documentation</a> and (if you really want to!) try it out!</p >
-            <div className={s.container}>
-            <img src="https://thewhaddonshowapi.azurewebsites.net/swagger/v1/swagger.json" alt="API Documentation" />
+            <div className={classnames(s.container,'clickable')}>
+                <img src={theWhaddonShowApiSnapshot} alt="API Documentation"/>
             </div>
         </div>
     )
