4a97684e836e955d207efdd4c150dcb52a3a3ed5
Mark Carter <magcarter@hotmail.co.uk>
2024-02-28 15:25:40 +0000

    try catch added to PersonsController

--- TheWhaddonShowReact/Controllers/PersonsController.cs
+++ TheWhaddonShowReact/Controllers/PersonsController.cs
@@ -9,70 +9,89 @@ using TheWhaddonShowReact.Models.LocalServer;
 
 namespace TheWhaddonShowReact.Controllers
 {
-	[Route("api/[controller]")]
-	[ApiController]
-	public class PersonsController : ControllerBase
-	{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonsController : ControllerBase
+    {
 
-		private readonly ReactLocalDataAccess<PersonUpdate> _localDataAccess;
-		private readonly ILocalServerEngine<PersonUpdate> _localServerEngine;
-		private readonly JsonSerializerOptions jsonOptions = new SyncJsonOptions().JsonOptions;
+        private readonly ReactLocalDataAccess<PersonUpdate> _localDataAccess;
+        private readonly ILocalServerEngine<PersonUpdate> _localServerEngine;
+        private readonly JsonSerializerOptions jsonOptions = new SyncJsonOptions().JsonOptions;
 
-		public PersonsController(ILocalDataAccess<PersonUpdate> localDataAccess, ILocalServerEngine<PersonUpdate> localServerEngine)
-		{
-			// Cast _localDataAccess to ReactLocalDataAccess<PersonUpdate>
-			if (localDataAccess is ReactLocalDataAccess<PersonUpdate> reactLocalDataAccess) //TODO: replace with IRemoteLocalDataAccess interface to avoid this hack.
-			{
-				_localDataAccess = reactLocalDataAccess;
-			}
-			else
-			{
-				throw new Exception("localDataAccess<T> specifiedin Dependency Injection is not of type ReactLocalDataAccess<T>." +
-					" This is required in order for the api controller to interact with React app correctly.");
-			}
-			_localServerEngine = localServerEngine;
-		}
-
-
+        public PersonsController(ILocalDataAccess<PersonUpdate> localDataAccess, ILocalServerEngine<PersonUpdate> localServerEngine)
+        {
+            // Cast _localDataAccess to ReactLocalDataAccess<PersonUpdate>
+            if (localDataAccess is ReactLocalDataAccess<PersonUpdate> reactLocalDataAccess) //TODO: replace with IRemoteLocalDataAccess interface to avoid this hack.
+            {
+                _localDataAccess = reactLocalDataAccess;
+            }
+            else
+            {
+                throw new Exception("localDataAccess<T> specifiedin Dependency Injection is not of type ReactLocalDataAccess<T>." +
+                    " This is required in order for the api controller to interact with React app correctly.");
+            }
+            _localServerEngine = localServerEngine;
+        }
 
 
-		// POST api/person/sync
-		[HttpPost("sync")]
-		public Task<IActionResult> Post([FromBody] LocalToServerSyncData<PersonUpdate> syncData)
-		{
-			if (syncData == null) return Task.FromResult<IActionResult>(BadRequest());
-
-			if (syncData != null)
-			{
-
-				_localDataAccess.setLocalToServerSyncData(syncData);
-
-				(DateTime? syncedDataTime, bool success) = _localServerEngine.TrySync().Result; //Can localserverEngine TrySync() be made async? (not sure)
 
 
-				if (success)
-				{
-					var serverToLocalSyncData = _localDataAccess.getServerToLocalSyncData();
+        // POST api/person/sync
+        [HttpPost("sync")]
+        public Task<IActionResult> Post([FromBody] LocalToServerSyncData<PersonUpdate> syncData)
+        {
+            try
+            {
+                if (syncData == null) return Task.FromResult<IActionResult>(BadRequest());
 
-					serverToLocalSyncData.Updates = serverToLocalSyncData.Updates.Where(update => update.IsSample == false).ToList();
+                if (syncData != null)
+                {
 
-					var serverToLocalSyncDataJson = JsonSerializer.Serialize(serverToLocalSyncData, jsonOptions);
+                    _localDataAccess.setLocalToServerSyncData(syncData);
 
-					return Task.FromResult<IActionResult>(Ok(serverToLocalSyncDataJson));
-				}
-				else
-				{
-					return Task.FromResult<IActionResult>(BadRequest());
-				}
-
-			}
-
-			return Task.FromResult<IActionResult>(Content("syncData is null"));
-
-		}
+                    (DateTime? syncedDataTime, bool success) = _localServerEngine.TrySync().Result; //Can localserverEngine TrySync() be made async? (not sure)
 
 
-	}
+                    if (success)
+                    {
+                        var serverToLocalSyncData = _localDataAccess.getServerToLocalSyncData();
+
+                        serverToLocalSyncData.Updates = serverToLocalSyncData.Updates.Where(update => update.IsSample == false).ToList();
+
+                        var serverToLocalSyncDataJson = JsonSerializer.Serialize(serverToLocalSyncData, jsonOptions);
+
+                        return Task.FromResult<IActionResult>(Ok(serverToLocalSyncDataJson));
+                    }
+                    else
+                    {
+                        return Task.FromResult<IActionResult>(BadRequest());
+                    }
+
+                }
+
+                return Task.FromResult<IActionResult>(Content("syncData is null"));
+
+
+            }
+            catch (Exception ex)
+            {
+                if (ex is UnauthorizedAccessException)
+                {
+                    return Task.FromResult<IActionResult>(Unauthorized());
+                }
+                else
+                {
+                    return Task.FromResult<IActionResult>(BadRequest((ex.Message)));
+                }
+
+            }
+
+
+
+        }
+
+
+    }
 
 }
 
